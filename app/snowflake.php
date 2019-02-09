0eddd593e710a17502b03ca5e03a52f4f9e11882
suzierandall <suzierandall@users.noreply.github.com>
2019-02-09 12:59:47 +0000

    Add set_grid_size method to store size

--- app/snowflake.php
+++ app/snowflake.php
@@ -3,6 +3,11 @@
 class Snowflake {
 	private $m_grid_size;
 
+	public function set_grid_size(int $size): bool {
+		$this->m_grid_size = $size;
+		return true;
+	}
+
 	public function get_grid_size() {
 		return $this->m_grid_size
 			?: $this->get_default_grid_size();
