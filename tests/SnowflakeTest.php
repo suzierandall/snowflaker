edc5a813915d7c95d01397cf685b0cd4f8f81c4f
suzierandall <suzierandall@users.noreply.github.com>
2019-02-09 02:52:13 +0000

    Setup autoloader bootstrap. Fix test class typos.

--- tests/SnowflakeTest.php
+++ tests/SnowflakeTest.php
@@ -1,9 +1,9 @@
 <?php
-require_once('/app/snowflake.php');
+require_once(__DIR__ . '/../app/snowflake.php');
 
 use PHPUnit\Framework\Testcase;
 
-class SnowflakeTest extends TestClass {
+class SnowflakeTest extends TestCase {
 	function testCanCreateSnowflake() {
 		$snowflake = new Snowflake;
 	}
---
+++ tests/bootstrap.php
@@ -0,0 +1,4 @@
+// Get autoloader
+require './vendor/autoload.php';
+
+
