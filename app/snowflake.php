3b1994a7762cc477c019e8cb76d9bfcd962059dd
suzierandall <suzierandall@users.noreply.github.com>
2019-02-09 13:09:36 +0000

    Implement class constructor, accept grid size parameter

--- app/snowflake.php
+++ app/snowflake.php
@@ -3,6 +3,12 @@
 class Snowflake {
 	private $m_grid_size;
 
+	public function __construct(int $size = null) {
+		if (is_int($size)) {
+			$this->set_grid_size($size);
+		}
+	}
+
 	public function set_grid_size(int $size): bool {
 		$this->m_grid_size = $size;
 		return true;
