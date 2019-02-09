9a2ea2fd4195fa8dd33acc8ced028b4fd677389c
suzierandall <suzierandall@users.noreply.github.com>
2019-02-09 13:14:40 +0000

    Add method descriptions and missing return type annotations

--- app/snowflake.php
+++ app/snowflake.php
@@ -3,23 +3,40 @@
 class Snowflake {
 	private $m_grid_size;
 
+	/**
+	 * Initialise the instance
+	 * @param int size - optionally, set a custom grid size
+	 */
 	public function __construct(int $size = null) {
 		if (is_int($size)) {
 			$this->set_grid_size($size);
 		}
 	}
 
+	/**
+	 * Set a custom grid size
+	 * @param int size - the custom grid size
+	 * @return bool - true
+	 */
 	public function set_grid_size(int $size): bool {
 		$this->m_grid_size = $size;
 		return true;
 	}
 
-	public function get_grid_size() {
+	/**
+	 * Get the grid size
+	 * @return int - custom or default grid size
+	 */
+	public function get_grid_size(): int {
 		return $this->m_grid_size
 			?: $this->get_default_grid_size();
 	}
 
-	public function get_default_grid_size() {
+	/**
+	 * Get the default grid size
+	 * @return int - the default grid size
+	 */
+	public function get_default_grid_size(): int {
 		return 9;
 	}
 }
