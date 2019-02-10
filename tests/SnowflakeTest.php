2c70153614f3a0b2415c2df72912de9f7dfcd038
suzierandall <suzierandall@users.noreply.github.com>
2019-02-10 04:32:17 +0000

    Promote grid-related methods into Grid class. Update tests to reflect.

--- tests/SnowflakeTest.php
+++ tests/SnowflakeTest.php
@@ -1,90 +1,90 @@
 <?php
-require_once(__DIR__ . '/../app/snowflake.php');
+require_once(__DIR__ . '/../app/grid.php');
 
 use PHPUnit\Framework\TestCase;
 
-class SnowflakeTest extends TestCase {
-	function testCanCreateSnowflake() {
-		$this->assertIsObject(new Snowflake);
+class GridTest extends TestCase {
+	function testCanCreateGrid() {
+		$this->assertIsObject(new Grid);
 	}
 
 	function testHasGridSize() {
-		$this->assertNotEmpty((new Snowflake)->get_grid_size());
+		$this->assertNotEmpty((new Grid)->get_grid_size());
 	}
 
 	function testHasDefaultGridSize() {
-		$this->assertSame(9, (new Snowflake)->get_default_grid_size());
+		$this->assertSame(9, (new Grid)->get_default_grid_size());
 	}
 
 	function testCanSetGridSize() {
-		$snowflake = new Snowflake;
-		$this->assertTrue($snowflake->set_grid_size(12));
-		$this->assertSame(12, $snowflake->get_grid_size());
+		$grid = new Grid;
+		$this->assertTrue($grid->set_grid_size(12));
+		$this->assertSame(12, $grid->get_grid_size());
 	}
 
 	function testCanSetGridSizeOnConstruction() {
-		$snowflake = new Snowflake(12);
-		$this->assertSame(12, $snowflake->get_grid_size());
+		$grid = new Grid(12);
+		$this->assertSame(12, $grid->get_grid_size());
 	}
 
 	function testHasGridSizeMax() {
-		$this->assertIsInt((new Snowflake)->get_grid_size_max());
+		$this->assertIsInt((new Grid)->get_grid_size_max());
 	}
 
 	function testHasGridSizeMin() {
-		$this->assertIsInt((new Snowflake)->get_grid_size_min());
+		$this->assertIsInt((new Grid)->get_grid_size_min());
 	}
 
 	function testCannotSetGridSizeSmallerThanMin() {
-		$this->assertFalse((new Snowflake)->set_grid_size(1));
+		$this->assertFalse((new Grid)->set_grid_size(1));
 	}
 
 	function testCannotSetGridSizeBiggerThanMax() {
-		$this->assertFalse((new Snowflake)->set_grid_size(50));
+		$this->assertFalse((new Grid)->set_grid_size(50));
 	}
 
 	function testCannotSetGridSizeOutsidePermittedRange() {
-		$snowflake = new Snowflake;
-		$this->assertFalse($snowflake->set_grid_size(-20));
-		$this->assertFalse($snowflake->set_grid_size(0));
-		$this->assertFalse($snowflake->set_grid_size(1));
-		$this->assertFalse($snowflake->set_grid_size(2));
-		$this->assertTrue($snowflake->set_grid_size(3));
-		$this->assertTrue($snowflake->set_grid_size(12));
-		$this->assertTrue($snowflake->set_grid_size(23));
-		$this->assertTrue($snowflake->set_grid_size(30));
-		$this->assertFalse($snowflake->set_grid_size(31));
-		$this->assertFalse($snowflake->set_grid_size(54));
-		$this->assertFalse($snowflake->set_grid_size(102));
+		$grid = new Grid;
+		$this->assertFalse($grid->set_grid_size(-20));
+		$this->assertFalse($grid->set_grid_size(0));
+		$this->assertFalse($grid->set_grid_size(1));
+		$this->assertFalse($grid->set_grid_size(2));
+		$this->assertTrue($grid->set_grid_size(3));
+		$this->assertTrue($grid->set_grid_size(12));
+		$this->assertTrue($grid->set_grid_size(23));
+		$this->assertTrue($grid->set_grid_size(30));
+		$this->assertFalse($grid->set_grid_size(31));
+		$this->assertFalse($grid->set_grid_size(54));
+		$this->assertFalse($grid->set_grid_size(102));
 	}
 
 	function testIsSizeWithinRange() {
-		$snowflake = new Snowflake;
-		$this->assertFalse($snowflake->is_size_within_range(-20));
-		$this->assertFalse($snowflake->is_size_within_range(0));
-		$this->assertFalse($snowflake->is_size_within_range(1));
-		$this->assertFalse($snowflake->is_size_within_range(2));
-		$this->assertTrue($snowflake->is_size_within_range(3));
-		$this->assertTrue($snowflake->is_size_within_range(12));
-		$this->assertTrue($snowflake->is_size_within_range(23));
-		$this->assertTrue($snowflake->is_size_within_range(30));
-		$this->assertFalse($snowflake->is_size_within_range(31));
-		$this->assertFalse($snowflake->is_size_within_range(54));
-		$this->assertFalse($snowflake->is_size_within_range(102));
+		$grid = new Grid;
+		$this->assertFalse($grid->is_size_within_range(-20));
+		$this->assertFalse($grid->is_size_within_range(0));
+		$this->assertFalse($grid->is_size_within_range(1));
+		$this->assertFalse($grid->is_size_within_range(2));
+		$this->assertTrue($grid->is_size_within_range(3));
+		$this->assertTrue($grid->is_size_within_range(12));
+		$this->assertTrue($grid->is_size_within_range(23));
+		$this->assertTrue($grid->is_size_within_range(30));
+		$this->assertFalse($grid->is_size_within_range(31));
+		$this->assertFalse($grid->is_size_within_range(54));
+		$this->assertFalse($grid->is_size_within_range(102));
 	}
 
 	function testDoesCapGridSizeOutOfRange() {
-		$snowflake = new Snowflake;
-		$this->assertSame(3, $snowflake->cap_size_within_range(-20));
-		$this->assertSame(3, $snowflake->cap_size_within_range(0));
-		$this->assertSame(3, $snowflake->cap_size_within_range(1));
-		$this->assertSame(3, $snowflake->cap_size_within_range(2));
-		$this->assertSame(3, $snowflake->cap_size_within_range(3));
-		$this->assertSame(12, $snowflake->cap_size_within_range(12));
-		$this->assertSame(23, $snowflake->cap_size_within_range(23));
-		$this->assertSame(30, $snowflake->cap_size_within_range(30));
-		$this->assertSame(30, $snowflake->cap_size_within_range(31));
-		$this->assertSame(30, $snowflake->cap_size_within_range(54));
-		$this->assertSame(30, $snowflake->cap_size_within_range(102));
+		$grid = new Grid;
+		$this->assertSame(3, $grid->cap_size_within_range(-20));
+		$this->assertSame(3, $grid->cap_size_within_range(0));
+		$this->assertSame(3, $grid->cap_size_within_range(1));
+		$this->assertSame(3, $grid->cap_size_within_range(2));
+		$this->assertSame(3, $grid->cap_size_within_range(3));
+		$this->assertSame(12, $grid->cap_size_within_range(12));
+		$this->assertSame(23, $grid->cap_size_within_range(23));
+		$this->assertSame(30, $grid->cap_size_within_range(30));
+		$this->assertSame(30, $grid->cap_size_within_range(31));
+		$this->assertSame(30, $grid->cap_size_within_range(54));
+		$this->assertSame(30, $grid->cap_size_within_range(102));
 	}
 }
